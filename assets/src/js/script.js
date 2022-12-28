a2989923ddbadb2f3f736df31bce59a17724714c
Kawan Wagnner <kawanwagnner7@gmail.com>
2022-12-28 17:00:32 -0300

    criação de layout

---
+++ assets/src/js/script.js
@@ -0,0 +1,11 @@
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('.container-txt', { 
+    rotate: {x: 1000, y: 20, z: 0}, duration: 2000 
+})
+
+sr.reveal('#logo', { duration: 2000 });
+
+sr.reveal('#navbar', { duration: 2000 });
+
+sr.reveal('#btn-produtos', { duration: 2000 });
